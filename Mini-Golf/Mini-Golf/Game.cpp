379c79020a41ac033261113b319d10a7994979ad
Daan05 <d.h.valkema@gmail.com>
2022-07-10 16:53:28 +0200

    LoadLevel()

--- Mini-Golf/Mini-Golf/Game.cpp
+++ Mini-Golf/Mini-Golf/Game.cpp
@@ -1,13 +1,13 @@
-#include "raylib.h"
 #include "Game.h"
-#include "Ball.h"
-#include "Hole.h"
+#include "raylib.h"
 
 bool Game::Run()
 {
     //Initialize components
     Ball ball(GetScreenWidth() / 2, GetScreenHeight() / 2, 10, WHITE); //These arguments are the initial position, radius and color of the ball
     Hole hole(16);
+
+    LoadLevel(1);
 
     while (!WindowShouldClose())
     {
@@ -30,4 +30,26 @@ void Game::Update(Ball &ball, Hole &hole)
 
     ball.Draw();
     ball.UpdatePosition();
+}
+
+void Game::LoadLevel(int level)
+{
+    std::ifstream lvlFile;
+    lvlFile.open("level1.txt");
+
+    std::vector<int> values;
+    std::string value;
+
+    while ( lvlFile )
+    {
+        lvlFile >> value;
+        values.push_back(stoi(value));
+    }
+    values.pop_back();
+
+    Ball ball(values.at(0), values.at(1), values.at(2), WHITE);
+    balls.push_back(ball);
+    Hole hole(values.at(3));
+    holes.push_back(hole);
+
 }
--- Mini-Golf/Mini-Golf/Game.h
+++ Mini-Golf/Mini-Golf/Game.h
@@ -1,11 +1,22 @@
 #pragma once
 #include "ball.h"
 #include "Hole.h"
+#include <vector>
+#include <fstream>
+#include <string>
+#include <iostream>
 
 class Game
 {
 public:
+	//std::vector<Obstacle> obstacles;
+
+	std::vector<Ball> balls;
+	std::vector<Hole> holes;
+
 	bool Run();
 
 	void Update(Ball& ball, Hole &hole);
+
+	void LoadLevel(int level);
 };
